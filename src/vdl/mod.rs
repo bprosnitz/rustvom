0987e92540d99f529ca7f0bca111c3a6691c038c
Benjamin Prosnitz <bprosnitz@google.com>
2016-03-04 21:43:29 -0800

    started list

--- src/vdl/mod.rs
+++ src/vdl/mod.rs
@@ -5,3 +5,7 @@ pub use self::vdltype::Kind;
 pub use self::vdltype::Field;
 pub use self::vdltype::Type;
 pub use self::target::Target;
+pub use self::target::ListTarget;
+pub use self::target::SetTarget;
+pub use self::target::MapTarget;
+pub use self::target::FieldsTarget;
--- src/vom/encoder.rs
+++ src/vom/encoder.rs
@@ -14,10 +14,68 @@ impl vdl::Target for encoder {
         }
     }
 
-    fn from_uint(&self, src: bool, tt: *mut vdl::Type) -> Option<io::Error> {
+    fn from_uint(&self, src: u64, tt: *mut vdl::Type) -> Option<io::Error> {
         match low_level_write::write_uint(self.writer, src) {
             Ok(n) => None,
             Err(err) => Some(err),
         }
     }
+
+    fn from_int(&self, src: i64, tt: *mut vdl::Type) -> Option<io::Error> {
+        match low_level_write::write_int(self.writer, src) {
+            Ok(n) => None,
+            Err(err) => Some(err),
+        }
+    }
+
+    fn from_float(&self, src: f64, tt: *mut vdl::Type) -> Option<io::Error> {
+        match low_level_write::write_float(self.writer, src) {
+            Ok(n) => None,
+            Err(err) => Some(err),
+        }
+    }
+
+    fn from_bytes(&self, src: &[u8], tt: *mut vdl::Type) -> Option<io::Error> {
+        match low_level_write::write_byte_slice(self.writer, src) {
+            Ok(n) => None,
+            Err(err) => Some(err),
+        }
+    }
+
+    fn from_string(&self, src: &str, tt: *mut vdl::Type) -> Option<io::Error> {
+        match low_level_write::write_string(self.writer, src) {
+            Ok(n) => None,
+            Err(err) => Some(err),
+        }
+    }
+
+    fn start_list(&self, tt: *mut vdl::Type, len: usize) -> Result<vdl::ListTarget, io::Error> {
+        match low_level_write::write_uint(self.writer, len) {
+            Ok(n) => Ok(&self),
+            Err(err) => return Err(err),
+        }
+    }
+
+    fn finish_list(&self, x: vdl::ListTarget) -> Option<io::Error> {
+        None
+    }
+}
+
+impl vdl::ListTarget for encoder {
+    fn start_elem(&self, index: usize) -> Result<vdl::Target, io::Error> {
+
+    }
+
+    fn finish_elem(&self, elem: vdl::Target) -> Option<io::Error> {
+
+    }
+}
+
+impl vdl::SetTarget for encoder {
+}
+
+impl vdl::MapTarget for encoder {
+}
+
+impl vdl::FieldsTarget for encoder {
 }
